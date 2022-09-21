0595637c0dd36997444d98a61a36bd886f198668
unknown <r26bisen@gmail.com>
2022-09-21 08:41:28 +0530

    view more button, footer

--- src/Components/PrimaryButton/PrimaryButton.jsx
+++ src/Components/PrimaryButton/PrimaryButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import primaryBtnStyle from './PrimaryButton.module.css'
+import  './PrimaryButton.css'
 export default function PrimaryButton({name,...props}) {
   return (
-    <button className={primaryBtnStyle.btn_primary} {...props}>{name}</button>
+    <button className={'btn_primary'} {...props}>{name}</button>
   )
 }
